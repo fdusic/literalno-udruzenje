52a9e5335c195c8b335e620379debfa0986c5d0f
Filip Dusic <filip.dusic@fuga.com>
2020-12-27 11:34:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# literalno-udruzenje
---
+++ src/main/java/com/upp/literalnoudruzenje/LiteralnoUdruzenjeApplication.java
@@ -0,0 +1,13 @@
+package com.upp.literalnoudruzenje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteralnoUdruzenjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteralnoUdruzenjeApplication.class, args);
+	}
+
+}
